259b1ecd63d5030b1140a4b283db46b32100d3c2
Hadeel Abdellatif <hadeel.abdellatif2001@gmail.com>
2024-05-14 13:53:41 +0300

    localhost url/employee/index -> to run the code

---
+++ Task_1/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task_1.Data;
+
+namespace Task_1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var emp = context.Employees.ToList();
+            return View("Index", emp);
+        }
+    }
+}
---
+++ Task_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task_1.Models;
+
+namespace Task_1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> Employees {  get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=Company_Employees;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
